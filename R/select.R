01ebfc26e45c0fbe91e5d38889c75a9106c21c8b
skuethe <crazycapivara@gmail.com>
2017-01-01 19:46:47 +0100

    Document and export SELECT func

--- R/select.R
+++ R/select.R
@@ -1,5 +1,17 @@
-SELECT <- imap_select <- function(imap, folder){
-  #imap$path <- curl_escape(folder) %>% gsub("%2F", "/", .)
+#' SELECT
+#'
+#' Select mailbox/folder.
+#'
+#' @param imap imap object
+#' @param folder mailbox/folder name
+#'
+#' @return updated imap object
+#' @export
+#'
+#' @examples
+#' imap <- IMAP("imaps://imap.gmail.com") %>% SELECT("INBOX")
+#'
+SELECT <- function(imap, folder){
   imap$path <- escape_url_(folder)
   imap
 }
