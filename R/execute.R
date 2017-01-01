ec9562710d9327a9a1c885676aa3d6c065faebbc
skuethe <crazycapivara@gmail.com>
2017-01-01 16:18:34 +0100

    Return headers as well

--- R/execute.R
+++ R/execute.R
@@ -3,6 +3,9 @@ execute <- function(imap){
   response <- curl::curl_fetch_memory(url, imap$handle)
   list(
     url = response$url,
-    content = rawToChar(response$content)
+    content = rawToChar(response$content),
+    headers = rawToChar(response$header)
+    #  (response$headers %>% rawToChar() %>%
+    #     textConnection() %>%  readLines())
   )
 }
