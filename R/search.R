0b1a37d161bfc757a30d96123f4ad313afd44663
skuethe <crazycapivara@gmail.com>
2017-01-01 20:07:43 +0100

    Document and export SEARCH func

--- R/search.R
+++ R/search.R
@@ -2,7 +2,22 @@ build_search <- function(){
   print("Not implemented yet!")
 }
 
-SEARCH <- imap_search <- function(imap, request = "ALL"){
+#' SEARCH
+#'
+#' Search mailbox for messages matching request.
+#'
+#' @param imap imap object
+#' @param request search request like 'ALL', 'UNSEEN', ...
+#'
+#' @return updated imap object
+#' @export
+#'
+#' @examples \dontrun{
+#'    # with 'imap' as an imap object ...
+#'    response <- imap %>%
+#'       SEARCH("UNSEEN") %>% execute()
+#' }
+SEARCH <- function(imap, request = "ALL"){
   curl::handle_setopt(
     imap$handle, customrequest = paste("SEARCH", request))
   imap
