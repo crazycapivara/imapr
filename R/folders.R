47c6c779fc87cda295f7f1d922c7d3b9e282631b
skuethe <crazycapivara@gmail.com>
2017-01-01 17:30:24 +0100

    Document and export LIST func

--- R/folders.R
+++ R/folders.R
@@ -1,4 +1,17 @@
-LIST <- imap_ls <- function(imap){
+#' LIST
+#'
+#' list folders on server
+#'
+#' @param imap imap object
+#'
+#' @return updated imap object
+#' @export
+#'
+#' @examples \dontrun{
+#'    # when 'imap' is an imap object ...
+#'    response <- imap %>% LIST() %>% execute()
+#' }
+LIST <- function(imap){
   imap$handle %>% curl::handle_setopt(customrequest = NULL)
   imap
 }
