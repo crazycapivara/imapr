481468d620e62d17d93d64ea942486c2fb69480b
skuethe <crazycapivara@gmail.com>
2017-01-01 15:56:15 +0100

    Add func to execute curl cmd

---
+++ R/execute.R
@@ -0,0 +1,8 @@
+execute <- function(imap){
+  url <- paste0(imap$url, "/", imap$path)
+  response <- curl::curl_fetch_memory(url, imap$handle)
+  list(
+    url = response$url,
+    content = rawToChar(response$content)
+  )
+}
