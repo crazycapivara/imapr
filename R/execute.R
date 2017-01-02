381662b3069cd52316418dc5404aed6b92804795
skuethe <crazycapivara@gmail.com>
2017-01-02 19:43:35 +0100

    Rewrite execute func in order to use new imap object

--- R/execute.R
+++ R/execute.R
@@ -13,17 +13,28 @@
 #'    response <- imap %>% LIST() %>% execute()
 #'    cat(response$content)
 #' }
-execute <- function(imap){
+execute_ <- function(imap){
   url <- paste0(imap$url, "/", imap$path)
   if(!is.null(imap$fetch)){
     url %<>% paste0(";", imap$fetch)
   }
   response <- curl::curl_fetch_memory(url, imap$handle)
   # reset custom request
-  curl::handle_setopt(imap$handle, customrequest = NULL)
+  #curl::handle_setopt(imap$handle, customrequest = NULL, url = NULL)
   list(
     url = response$url,
     content = rawToChar(response$content),
     headers = rawToChar(response$header)
+  )
+}
+
+execute <- function(imap){
+  url <- paste0(imap$url, "/", imap$path)
+  handle = curl::new_handle()
+  curl::handle_setopt(handle, .list = imap$options)
+  response <- curl::curl_fetch_memory(imap$url, handle = handle)
+  list(
+    url = response$url,
+    content = rawToChar(response$content)
   )
 }
