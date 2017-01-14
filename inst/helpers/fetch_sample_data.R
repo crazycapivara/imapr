90619972da1337641a1e02c4c6cfbcd0213cce87
skuethe <crazycapivara@gmail.com>
2017-01-14 11:24:06 +0100

    Update helper func to fetch sample data

--- inst/helpers/fetch_sample_data.R
+++ inst/helpers/fetch_sample_data.R
@@ -1,6 +1,12 @@
 host <- "imaps://imap.gmail.com"
 
+save_response <- function(x, filename){
+  saveRDS(x, filename)
+  sprintf("response saved to %s", filename)
+}
+
 fetch_sample_data <- function(username, password){
   imap <- IMAP(host) %>% user(username, password)
-  imap %>% SELECT("INBOX") %>% FETCH(1) %>% execute()
+  imap %>% SELECT("INBOX") %>% SEARCH("ALL") %>%
+    execute() %>% save_response("tmp/data/response-search_all.rds")
 }
