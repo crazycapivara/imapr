f4602d674a342a70257129b39d49d775268e311c
skuethe <crazycapivara@gmail.com>
2017-01-15 20:05:32 +0100

    Update python script to parse header

--- inst/python/mailparser.py
+++ inst/python/mailparser.py
@@ -1,15 +1,16 @@
 import sys
-import email
+import json
+from email import message_from_file
+from email.header import decode_header, make_header
 
 def parse(filename):
   with open(filename) as fp:
-    msg = email.message_from_file(fp)
-  return msg
+    msg = message_from_file(fp)
+  fields = ["from", "to", "date", "subject"]
+  header = {field: str(make_header(decode_header(msg[field]))) for field in fields}
+  return header
 
 if __name__ == "__main__":
-  print("parse that")
-  #print(sys.argv[1])
-  msg = parse("/tmp/RtmpeSLRPH/file60f57e51d2")
-  print(msg["date"])
-  print(msg.keys())
-  print(msg.items())
+  #print("parse that")
+  header = parse(sys.argv[1])
+  print(json.dumps(header, ensure_ascii = False))
