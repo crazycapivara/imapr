17babb378d1b3b2e955b728cf767407d0c432386
skuethe <crazycapivara@gmail.com>
2017-01-19 20:10:44 +0100

    Add documentation for pipe operator from magrittr

--- R/package.R
+++ R/package.R
@@ -1,9 +1,19 @@
 #' imapr - An R Interface to IMAP
-#' 
+#'
 #' Performs IMAP queries via curl.
-#' 
+#'
 #' @name imapr
-#' 
+#'
 #' @importFrom magrittr %>% %<>%
 #' @export %>%
+#'
+NULL
+
+#' Pipe operator.
+#'
+#' Exported from \code{\pkg{magrittr}}.
+#'
+#' @aliases %>%
+#' @name pipe
+#'
 NULL
