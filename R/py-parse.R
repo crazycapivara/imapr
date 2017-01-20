1c1a9efa2e8b71536bda4f1e32a79607ab44e410
skuethe <crazycapivara@gmail.com>
2017-01-20 20:37:15 +0100

    Add func using python script to parse email body

--- R/py-parse.R
+++ R/py-parse.R
@@ -3,3 +3,9 @@ py_parse_header <- function(filename){
   system2("python", c(script_name, filename), stdout = TRUE) %>%
     jsonlite::fromJSON()
 }
+
+py_parse_body <- function(filename){
+  script_name <- system.file("python/bodyparser.py", package = "imapr")
+  system2("python", c(script_name, filename), stdout = TRUE) %>%
+    jsonlite::fromJSON()
+}
