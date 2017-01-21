2d642c70efd4d05519316599494156c755064030
skuethe <crazycapivara@gmail.com>
2017-01-21 17:14:07 +0100

    Update func to execute python scripts

--- R/lowlevel.R
+++ R/lowlevel.R
@@ -1,9 +1,15 @@
+# REMOVE!?
 set_options <- function(imap, ...){
   cat("NOT implemented yet!")
 }
 
-exec_python <- function(argv){
+get_python_script_name <- function(script_name){
+  sprintf("python/%s", script_name) %>%
+    system.file(package = "imapr")
+}
+
+exec_python_script <- function(script_name, argv){
   cmd <- get_python_cmd()
-  system2(cmd, argv, stdout = TRUE) %>%
+  system2(cmd, c(script_name, argv), stdout = TRUE) %>%
     jsonlite::fromJSON()
 }
