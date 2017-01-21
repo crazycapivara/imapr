b97ac39ad9dbe016d221cbe9ff0c13c422cbe7bd
skuethe <crazycapivara@gmail.com>
2017-01-21 16:54:34 +0100

    Add func to exec python code

---
+++ R/lowlevel.R
@@ -0,0 +1,9 @@
+set_options <- function(imap, ...){
+  cat("NOT implemented yet!")
+}
+
+exec_python <- function(argv){
+  cmd <- get_python()
+  system2(cmd, argv, stdout = TRUE) %>%
+    jsonlite::fromJSON()
+}
