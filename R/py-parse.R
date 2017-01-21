c5f729f05eb5e3230bffa55f029eff455161baac
skuethe <crazycapivara@gmail.com>
2017-01-21 17:20:15 +0100

    Document and export py_parse_header func

--- R/py-parse.R
+++ R/py-parse.R
@@ -1,3 +1,19 @@
+#' Parser email header.
+#'
+#' Parses email header using python.
+#'
+#' @param filename filename of raw message
+#'
+#' @return list with (to, from, date, subject)
+#' @export
+#'
+#' @examples \dontrun{
+#'    imap <- IMAP("server") %>% user("uid", "pwd") %>%
+#'       SELECT("INBOX")
+#'    imap %>% FETCH(1) %>% execute(tempfile()) %>%
+#'       py_parse_header()
+#' }
+#'
 py_parse_header <- function(filename){
   get_python_script_name("mailparser.py") %>%
     exec_python_script(filename)
