3e2f7f65464357ad7f7a7496d77a70b8a15f82e7
skuethe <crazycapivara@gmail.com>
2017-01-25 20:02:45 +0100

    Update py_parse

--- R/py-parse.R
+++ R/py-parse.R
@@ -15,11 +15,11 @@
 #' }
 #'
 py_parse_header <- function(filename){
-  get_python_script_name("mailparser.py") %>%
-    exec_python_script(filename)
+  get_python_script_name("parser.py") %>%
+    exec_python_script(c(filename, "--header"))
 }
 
 py_parse_body <- function(filename){
-  get_python_script_name("bodyparser.py") %>%
+  get_python_script_name("parser.py") %>%
     exec_python_script(filename)
 }
