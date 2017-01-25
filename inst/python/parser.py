22340584031926ffb9bb7faedc6525d46988542f
skuethe <crazycapivara@gmail.com>
2017-01-25 19:47:04 +0100

    Refactor python mail parser

--- inst/python/parser.py
+++ inst/python/parser.py
@@ -1,11 +1,12 @@
-import sys
+import argparse
 import json
 from email import message_from_file
+from email.header import decode_header, make_header
 
 def read_msg(filename):
   with open(filename) as fp:
     msg = message_from_file(fp)
-  return(msg)
+  return msg
 
 def _get_payload(part):
   charset = part.get_content_charset()
@@ -21,7 +22,11 @@ def walk(msg):
       data["html"] += _get_payload(part)
   return data
 
-def parse(msg):
+def parse_header(msg):
+  fields = ["from", "to", "date", "subject"]
+  return {field: str(make_header(decode_header(msg[field]))) for field in fields}
+
+def parse_body(msg):
   if msg.is_multipart():
     data = walk(msg)
   else:
@@ -29,7 +34,18 @@ def parse(msg):
     data = _get_payload(msg)
   return(data)
 
+def get_argv():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("filename")
+  parser.add_argument("--header", action = "store_true")
+  return parser.parse_args()
+
 if __name__ == "__main__":
-  msg = read_msg(sys.argv[1])
-  data = parse(msg)
+  argv = get_argv()
+  #print(argv)
+  msg = read_msg(argv.filename)
+  if argv.header:
+    data = parse_header(msg)
+  else:
+    data = parse_body(msg)
   print(json.dumps(data, ensure_ascii = False))
