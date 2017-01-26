212ef534c2e23da3f2ab015bc38a55fdf36104db
skuethe <crazycapivara@gmail.com>
2017-01-26 21:48:13 +0100

    Add section parameter to sections func

--- R/section.R
+++ R/section.R
@@ -1,17 +1,28 @@
 #' List section options.
 #'
+#' @param select section to be returned
+#'
 #' @return list of section options
 #' @export
 #'
 #' @seealso \code{\link{FETCH}}
 #'
 #' @examples
-#' sections()$header_min
-sections <- function(){
-  list(
+#' # show options
+#' sections()
+#'
+#' select 'header_min'
+#' sections(header_min)
+#'
+sections <- function(select = NULL){
+  sections_ <- list(
     all = "",
     header = "HEADER",
     header_min = "HEADER.FIELDS%20(DATE%20FROM%20TO%20SUBJECT)",
     body = "TEXT"
   )
+  if(!is.null(select)){
+    return(sections_[[select]])
+  }
+  sections_
 }
