452c49b960861071a3932a9d89ec949d91c24e37
skuethe <crazycapivara@gmail.com>
2017-02-18 15:34:30 +0100

    Add startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,1 +1,9 @@
 globalVariables(".")
+
+.onAttach <- function(libname, pkgname){
+  packageStartupMessage(
+    pkgname, " ", getNamespaceVersion(pkgname), "\n",
+    "   Perform IMAP queries via curl\n",
+    "   Issues, notes and quick start: https://github.com/crazycapivara/iampr/\n"
+  )
+}
