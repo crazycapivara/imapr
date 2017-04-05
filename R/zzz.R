7f3b79089b45e4cdf8fe4a5a832942a07d41fcd1
skuethe <crazycapivara@gmail.com>
2017-04-05 18:34:40 +0200

    Update startup message

--- R/zzz.R
+++ R/zzz.R
@@ -4,6 +4,6 @@ globalVariables(".")
   packageStartupMessage(
     pkgname, " ", getNamespaceVersion(pkgname), "\n",
     "   Perform IMAP queries via curl\n",
-    "   Issues, notes and quick start: https://github.com/crazycapivara/iampr/\n"
+    "   Issues, notes and quick start: https://github.com/crazycapivara/iampr/"
   )
 }
